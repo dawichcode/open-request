6348cf4c11f3e071b15bfea41f5f4970d1bc1b09
wcodewiz <samuelclinton975@gmail.com>
2024-08-21 20:44:25 +0100

    finished current update and major bug fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import useAjax from "./hooks/useAjax.tsx";
 import {getBrowserInfo,isEmpty,encryptToken,decryptToken,dateExpired} from "./utils/utils.tsx";
 import useOnce from "./hooks/useOnce.tsx";
+import httpClient from "./utils/httpClient.tsx";
 
-export {getBrowserInfo,useOnce, isEmpty, useAjax,encryptToken as encrypt,decryptToken as decrypt,dateExpired}
+export {getBrowserInfo,useOnce, isEmpty, useAjax,encryptToken as encrypt,decryptToken as decrypt,dateExpired,httpClient}
